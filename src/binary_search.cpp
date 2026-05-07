964e38dc00008c195c70344451ebb49d2ad2a095
Pauuvl <159390787+Pauuvl@users.noreply.github.com>
2026-05-06 21:06:36 -0500

    Correcciones de la busqueda binaria

--- src/binary_search.cpp
+++ src/binary_search.cpp
@@ -1,18 +1,19 @@
 #include "binary_search.hpp"
 
-
 int busquedaBinaria(const std::vector<Solicitud>& arr, int izq, int der, int k) {
-    
-    if (izq > der) return -1;
+
+    if (izq > der)
+        return -1;
 
     int mid = izq + (der - izq) / 2;
 
-    if (arr[mid].tenure >= k) {
-        
-        int resultado = busquedaBinaria(arr, izq, mid - 1, k);
-        return (resultado == -1) ? mid : resultado;
-    } else {
-    
+    // Encontró exactamente el tenure buscado
+    if (arr[mid].tenure == k)
+        return mid;
+
+    // Como está ordenado DESCENDENTE
+    if (arr[mid].tenure < k)
         return busquedaBinaria(arr, izq, mid - 1, k);
-    }
+
+    return busquedaBinaria(arr, mid + 1, der, k);
 }
